643d6f7a414d7d0cc997adbc0c74bd74ee46e5a7
solc1us <cyan.magina@gmail.com>
2026-03-28 20:05:40 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022400025/Program.cs
+++ TP_MODUL5_103022400025/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MODUL5_103022400025;
+
+Console.WriteLine("Masukkan NIM:");
+
+// Masukkan NIM
+string nim = Console.ReadLine();
+
+// Membuat objek DataGeneric dengan tipe data string
+DataGeneric<string> dataNIM = new DataGeneric<string>(nim);
+
+// Menampilkan data NIM yang tersimpan
+dataNIM.printData();
